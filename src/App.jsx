24fd0930e1558bc8b9bbc51107e0835ad4bb1a22
ukrWily <ukrwily@gmail.com>
2024-09-10 13:56:56 +0300

    L props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
-import JournalItem from "./modules/JournalItem/JournalItem";
+import JournalItem from "./modules/JournalItem/JournalItem.jsx";
 import Button from "./modules/Button/Button.jsx";
 
 function App() {
+  const data = [
+    {
+      title: "Title",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.",
+      date: new Date(),
+    },
+    {
+      title: "Title two",
+      text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.`,
+      date: new Date(),
+    },
+  ];
   return (
     <>
+      <h1>Title</h1>
+      <p>Some text</p>
       <Button />
       <JournalItem />
     </>
--- src/modules/JournalItem/JournalItem.jsx
+++ src/modules/JournalItem/JournalItem.jsx
@@ -3,14 +3,15 @@ import "./JournalItem.css";
 function JournalItem() {
   const title = "Sometitle";
   const date = new Date();
-  const text = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
-    `;
+  const text =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.";
   return (
     <div className="journal-item">
       <h2 className="journal-item__header">{title}</h2>
       <div className="journal-item__body">
-        <div className="journal-item__date">{date}</div>
+        <div className="journal-item__date">
+          {new Intl.DateTimeFormat("ua-UA").format(new Date())}
+        </div>
         <div className="journal-item__text">{text}</div>
       </div>
     </div>

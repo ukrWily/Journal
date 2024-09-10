8b736cc512591b7a0b8b4c184f81d5abe76b8539
ukrWily <ukrwily@gmail.com>
2024-09-10 12:14:45 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import JournalItem from "./modules/Button/Button";
+import JournalItem from "./modules/JournalItem/JournalItem";
+import Button from "./modules/Button/Button.jsx";
 
 function App() {
   return (
     <>
+      <Button />
       <JournalItem />
     </>
   );
--- src/modules/JournalItem/JournalItem.jsx
+++ src/modules/JournalItem/JournalItem.jsx
@@ -1,11 +1,17 @@
+import "./JournalItem.css";
 //
 function JournalItem() {
+  const title = "Sometitle";
+  const date = new Date();
+  const text = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
+    `;
   return (
     <div className="journal-item">
-      <h2 className="journal-item__header">1</h2>
+      <h2 className="journal-item__header">{title}</h2>
       <div className="journal-item__body">
-        <div className="journal-item__date">2</div>
-        <div className="journal-item__text">3</div>
+        <div className="journal-item__date">{date}</div>
+        <div className="journal-item__text">{text}</div>
       </div>
     </div>
   );

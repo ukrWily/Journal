9d9d339985f281b5b3c202adaf3fb47d7cd64e67
ukrWily <ukrwily@gmail.com>
2024-09-24 12:23:13 +0300

    L setItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import JournalItem from "./components/JournalItem/JournalItem.jsx";
 import CardButton from "./components/CardButton/CardButton.jsx";
@@ -8,20 +9,33 @@ import JournalList from "./components/JournalList/JournalList.jsx";
 import JournalAddButton from "./components/JournalAddButton/JournalAddButton.jsx";
 import JournalForm from "./components/JournalForm/JournalForm.jsx";
 
-function App() {
-  const data = [
-    {
-      title: "Title",
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.",
-      date: new Date(),
-    },
-    {
-      title: "Title two",
-      text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
+const INITIAL_DATA = [
+  {
+    title: "Title",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.",
+    date: new Date(),
+  },
+  {
+    title: "Title two",
+    text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus error vero dignissimos, sit ratione id odit labore, necessitatibus maiores, accusantium nulla nisi ut quis quae facilis doloremque in cum perspiciatis.`,
-      date: new Date(),
-    },
-  ];
+    date: new Date(),
+  },
+];
+
+function App() {
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = (item) => {
+    setItems((oldItems) => [
+      ...oldItems,
+      {
+        title: item.title,
+        text: item.text,
+        date: new Date(item.date),
+      },
+    ]);
+  };
 
   return (
     <div className="app">
@@ -29,7 +43,7 @@ function App() {
         <Header />
         <JournalAddButton />
         <JournalList>
-          {data.map((el, i) => (
+          {items.map((el, i) => (
             <CardButton key={i}>
               <JournalItem title={el.title} text={el.text} date={el.date} />
             </CardButton>
@@ -37,7 +51,7 @@ function App() {
         </JournalList>
       </LeftPanel>
       <Body>
-        <JournalForm />
+        <JournalForm onSubmit={addItem} />
       </Body>
     </div>
   );
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
 import "./JournalForm.css";
 import Button from "../Button/Button.jsx";
 
-const JournalForm = () => {
-  const [inputData, setInputData] = useState("");
-
-  const inputChange = (event) => {
-    setInputData(event.target.value);
-    console.log(inputData);
-  };
-
+const JournalForm = ({ onSubmit }) => {
   const addJournalItem = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
+    onSubmit(formProps);
   };
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
       <input type="text" name="title" />
       <input type="date" name="date" />
-      <input type="text" name="tag" value={inputData} onChange={inputChange} />
-      <textarea name="post"></textarea>
+      <input type="text" name="tag" />
+      <textarea name="text"></textarea>
       <Button
         text="Save"
         onClick={() => {
